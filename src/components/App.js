be5b339b5bac8faa407f8b6e3dd8d66ffeccea44
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-13 13:33:58 +0530

    Functional Component Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showPara, setShowPara] = useState(false); // used to declare our own State variables in function, can't be used in class component
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onclick={() => setShowPara(!showPara)}>Toggle Para</button>
+      {showPara ? (<p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>) : null}
     </div>
   );
 }
